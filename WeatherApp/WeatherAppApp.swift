8e8cb25973d1ca8f60b972b77d5770513324f2bb
Blessing Mabunda <blessingmabunda244@gmail.com>
2026-04-01 17:33:12 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Blessing Mabunda on 2026/04/01.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
